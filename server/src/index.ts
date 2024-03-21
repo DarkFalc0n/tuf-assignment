476f047140ad1ba1801ee4ebdee1305aca930a95
DarkFalc0n <pratyayroy20@gmail.com>
2024-03-21 23:03:08 +0530

    fix: address cors issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,18 @@ import express from "express";
 import { getDbConnection } from "./db/connection";
 import dotenv from "dotenv";
 import { submitCode, fetch } from "./routes";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
 const port = process.env.APP_LOCAL_PORT || 10000;
 
 app.get("/", (_, res) => {
   res.send("Hello from TUF backend");
 });
+
+app.use(cors());
 
 //code submission route
 app.use("/submit", submitCode);
